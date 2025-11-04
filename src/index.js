03c708d04be9810df2ca819a136775cafb8c49cc
skyfire-supreet <supreet@skyfire.xyz>
2025-11-03 21:02:01 -0500

    SKYK-734 adds validations + bid logging + charging logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import * as jws from "jws";
 import { KVStore } from "fastly:kv-store";
-
-const sessionDuration = 300; //Number(process.env.REDIS_SESSION_EXPIRY) ||
-const perRequestAmount = 0.001; //Number(jwtPayload.spr) || 0;
+var validator = require("validator");
 
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
@@ -15,34 +13,7 @@ const isBotRequest = (req) => {
   return req.headers.get("x-isbot") === "true";
 };
 
-// const getDecodedJWT = (token) => {
-//   console.log("token", token);
-//   console.log("isJWT(token)", isJWT(token));
-//   if (isJWT(token)) {
-//     const jwtHeader = jwtDecode(token, { header: true });
-//     const jwtPayload = jwtDecode(token);
-
-//     const jwtDecoded = { header: jwtHeader, payload: jwtPayload };
-//     console.log("jwtHeader", jwtHeader);
-//     console.log("jwtPayload", JSON.stringify(jwtPayload));
-//     console.log("jwtDecoded", jwtDecoded);
-
-//     return {
-//       isValid: true,
-//       jwtPayload,
-//     };
-//   } else {
-//     console.log({ err }, "Error while verifying token: ");
-
-//     return {
-//       isValid: false,
-//       errorMessage: "Something went wrong while verifying your JWT token",
-//       errorStatusCode: 401,
-//     };
-//   }
-// };
-
-const SKYFIRE_SELLER_API_KEY = "8987b55a-44f7-4f64-ab8e-1ff76663b03c"; //6c0217fa-b746-4db1-9ab1-292203d9e8af
+const SKYFIRE_SELLER_API_KEY = "6c0217fa-b746-4db1-9ab1-292203d9e8af"; //8987b55a-44f7-4f64-ab8e-1ff76663b03c
 
 async function chargeToken(skyfireToken, amountToCharge = "0.0001") {
   try {
@@ -68,8 +39,13 @@ async function chargeToken(skyfireToken, amountToCharge = "0.0001") {
     console.log("response", response);
     const data = await response.json();
 
-    if (data.code === "PAYMENT_ERROR") {
-      throw new Error(`Payment Error: ${data.code} ${data.message}`);
+    console.log("data", data);
+    console.log("typeof(data)", typeof data);
+
+    if (data.code) {
+      throw new Error(
+        `Error while token charging: ${data.code} ${data.message}`
+      );
     }
 
     console.log({
@@ -92,9 +68,10 @@ const jwtSecret = `-----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEN1pqyHAt02vpYLoiXSjZPvPjJwZV
 VNfI7YZvgsXBbPBISDhhMTSppRO6ts366lq1pYyNYQQZE0kvFThRVhptZw==
 -----END PUBLIC KEY-----`;
+
 // Check for the custom header
-async function verifySkyfirePayIdHeader(skyfireToken, redis) {
-  // Only decode token if request is from a bot
+async function verifySkyfirePayIdHeader(skyfireToken) {
+  // Only verify & decode token if request is from a bot
   try {
     if (jws.verify(skyfireToken, "ES256", jwtSecret)) {
       const jwtPayload = JSON.parse(
@@ -103,14 +80,94 @@ async function verifySkyfirePayIdHeader(skyfireToken, redis) {
 
       const jwtHeader = jws.decode(skyfireToken, "ES256", jwtSecret).header;
 
-      if (jwtHeader.typ === "kya+pay+JWT") {
+      if (!["kya+JWT", "kya+pay+JWT", "pay+JWT"].includes(jwtHeader.typ)) {
+        console.error(`Validation failed: typ should be one of kya+JWT or pay+JWT or kya+pay+JWT`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: typ should be one of kya+JWT or pay+JWT or kya+pay+JWT",
+          errorStatusCode: 401,
+        };
+      }
+
+      // email should be correct format
+      if (!validator.isEmail(String(jwtPayload.bid.skyfireEmail))) {
+        console.error(`Validation failed: Invalid email format in 'bid.skyfireEmail' claim`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: Invalid email format in 'bid.skyfireEmail' claim",
+          errorStatusCode: 401,
+        };
+      }
+      
+      // env should be qa
+      if (jwtPayload.env !=="qa") {
+        console.error(`Validation failed: Token is not from QA environment`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: Token is not from QA environment",
+          errorStatusCode: 401,
+        };
+      }
+      
+      // jti should be a valid uuid
+      if (!validator.isUUID(String(jwtPayload.jti))) {
+        console.error(`Validation failed: Invalid token ID (jti)`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: Invalid token ID (jti)",
+          errorStatusCode: 401,
+        };
+      }
+
+      // sub should be a valid uuid
+      if (!validator.isUUID(String(jwtPayload.sub))) {
+        console.error(`Validation failed: Invalid subject (sub)`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: Invalid subject (sub)",
+          errorStatusCode: 401,
+        };
+      }
+
+      // aud should be a valid uuid
+      if (!validator.isUUID(String(jwtPayload.aud))) {
+        console.error(`Validation failed: Invalid audience (aud)`);
+        return {
+          isValid: false,
+          errorMessage: "Validation failed: Invalid audience (aud)",
+          errorStatusCode: 401,
+        };
+      }
+
+       if (["kya+JWT", "kya+pay+JWT"].includes(jwtHeader.typ)) {
+        // log buyer identity in KV store
         await new KVStore("first_KV_batch_charging").put(
           jwtPayload?.bid?.skyfireEmail,
           Date.now()
         );
       }
+
+      if (["pay+JWT", "kya+pay+JWT"].includes(jwtHeader.typ)) {
+        // charge token here
+        try {
+          let { amountCharged, remainingBalance } = await chargeToken(
+            skyfireToken
+          );
+          console.log("amountCharged from token - ", amountCharged);
+          console.log("remainingBalance from token - ", remainingBalance);
+        } catch (err) {
+          console.error("Error while charging token: ", err);
+          return {
+            isValid: false,
+            errorMessage: "Payment Required: Error charging Token.",
+            errorStatusCode: 402,
+          };
+        }
+      }
+
       return {
         isValid: true,
+        jwtPayload,
       };
     } else {
       return {
@@ -119,8 +176,8 @@ async function verifySkyfirePayIdHeader(skyfireToken, redis) {
         errorStatusCode: 401,
       };
     }
-    // const { jwtPayload } = getDecodedJWT(skyfireToken);
   } catch (err) {
+    console.error("Error while verifying token: ", { err });
     return {
       isValid: false,
       errorMessage: "Something went wrong while verifying your JWT token",
@@ -132,33 +189,9 @@ async function verifySkyfirePayIdHeader(skyfireToken, redis) {
 async function handleRequest(event) {
   const req = event.request;
 
-  // const redis = new Redis({
-  //   url: "https://sure-seasnail-19324.upstash.io", //upstash url
-  //   token: "AUt8AAIncDI3Y2E3YmI0NzQyZDY0OWUxYTNiMzZkMzQ4NjVhYjNmNHAyMTkzMjQ", // upstash token
-  //   backend: "upstash",
-  // });
-  // const data = await redis.incr("count");
-  // // return new Response("View Count:" + data, { status: 200 });
-  // console.log("View Count:" + data);
-
-  const store = new KVStore("first_KV_batch_charging");
-
-  const firstresult = await store.get("first");
-  console.log("firstresult", await firstresult.text());
-
   console.log("isBotRequest(req)", isBotRequest(req));
 
   if (isBotRequest(req)) {
-    console.log(
-      "req.headers.get(skyfire-pay-id)",
-      req.headers.get("skyfire-pay-id")
-    );
-
-    console.log(
-      "req.headers.get(skyfire-pay-id) === null",
-      req.headers.get("skyfire-pay-id") === null
-    );
-
     if (
       req.headers.get("skyfire-pay-id") === null ||
       req.headers.get("skyfire-pay-id") === "" ||
@@ -176,8 +209,7 @@ async function handleRequest(event) {
     }
 
     skyfirePayIdVerificationRes = await verifySkyfirePayIdHeader(
-      req.headers.get("skyfire-pay-id"),
-      redis
+      req.headers.get("skyfire-pay-id")
     );
     console.log("skyfirePayIdVerificationRes", skyfirePayIdVerificationRes);
 
@@ -188,68 +220,29 @@ async function handleRequest(event) {
         headers: {},
       });
     } else {
-      const newReq = new Request(req, {
-        headers: new Headers(req.headers),
-      });
-      let skyfireToken = req.headers.get("skyfire-pay-id");
-      // let { amountCharged, remainingBalance } = await chargeToken(
-      //   skyfireToken
-      // );
-
-      // console.log("amountCharged from token - ", amountCharged);
-      // console.log("remainingBalance from token - ", remainingBalance);
-
-      // let usageRes = await usageTrack(
-      //   skyfireToken,
-      //   getDecodedJWT(skyfireToken),
-      //   redis
-      // );
-      // console.log("usageRes", usageRes);
-
-      // if (usageRes.isError) {
-      //   return usageRes.errorResponse;
-      // }
-
-      // console.log("usaegRes paymentHeaders", usageRes.paymentHeaders);
-      // Send request to backend (configured as 'origin_0')
-      const beresp = await fetch(newReq, {
-        backend: "real_estate_protected_website",
-      });
-
-      const respBody = await beresp.arrayBuffer();
-
-      const mergedHeaders = new Headers(beresp.headers);
-
-      // Add payment headers
-      // for (const [key, value] of Object.entries(usageRes.paymentHeaders)) {
-      //   mergedHeaders.set(key, value);
-      // }
-       const bid = await new KVStore("first_KV_batch_charging").get(
-      "supreet@skyfire.xyz"
-    );
-    console.log("buyer identity", await bid.text());
-
-      return new Response(respBody, {
-        status: beresp.status,
-        statusText: beresp.statusText,
-        headers: mergedHeaders,
-      });
+      //TODO: only for testing - remove later - start
+      const bid = await new KVStore("first_KV_batch_charging").get(
+        "supreet@skyfire.xyz"
+      );
+      console.log("buyer identity", await bid.text());
+      //TODO: only for testing - remove later - end
     }
-  } else {
-    const newReq = new Request(req, {
-      headers: new Headers(req.headers),
-    });
-
-    const beresp = await fetch(newReq, {
-      backend: "real_estate_protected_website",
-    });
-
-    const respBody = await beresp.arrayBuffer();
-
-    return new Response(respBody, {
-      status: beresp.status,
-      statusText: beresp.statusText,
-      headers: beresp.headers,
-    });
   }
+
+  // Send request to backend (configured as 'origin_0')
+  const newReq = new Request(req, {
+    headers: new Headers(req.headers),
+  });
+
+  const beresp = await fetch(newReq, {
+    backend: "real_estate_protected_website",
+  });
+
+  const respBody = await beresp.arrayBuffer();
+
+  return new Response(respBody, {
+    status: beresp.status,
+    statusText: beresp.statusText,
+    headers: beresp.headers,
+  });
 }
